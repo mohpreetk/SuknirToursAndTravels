ecad6c2cdbcee81776215d2d12fd37f18d8975bd
Mohpreet Kaur <67534712+mohpreetk@users.noreply.github.com>
2020-07-20 00:57:25 -0400

    Completed website
    Changes are always welcome under learning process

---
+++ process-form.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>Saved - Suknir Tours and Travels</title>
+		<link href="css/styles.css" rel="stylesheet">
+    <link href="css/stylesindex.css" rel="stylesheet">
+  </head>
+  <body>
+    <header>
+      <img src="images/logo.jpg" alt="Logo" >
+      <div class="header">
+      <h1>suknir tours and travels</h1>
+      <h2>explore nature, explore himachal</h2>
+      <nav>
+        <ul>
+          <li><a href="index.html" title="Go to home page">Home</a></li>
+          <li><a href="contact.html" title="Go to contact page">Contact Us</a></li>
+          <li><a href="contact.html#legal" title="Go to legal information">Legal information</a></li>
+          <li><a href="index.html#destinations" title="Go to popular tourist places of himachal">Popular Tourist Destinations</a></li>
+        </ul>
+      </nav>
+      </div>
+      <img src="images/logo.jpg" alt="Logo" >
+    </header>
+		<main>
+			<div class="banner">
+				<h3>We will return to you soon!</h3>
+			</div>
+				<?php
+					echo('<p class="message">Thank You, your question has reached us.</p>');
+				?>
+			</main>
+	    <footer>
+	      <small>©2020 All rights reserved.</small>
+	    </footer>
+	  </body>
+	</html>
